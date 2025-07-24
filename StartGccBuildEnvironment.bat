615ba0574fa0969da083ae1673323089ca8b4e47
Schwabano <tim.zierer@gmail.com>
2025-07-24 17:52:59 -0600

    Update build environment from IMXRT1060-EVK-BuildEnvironment-V14.00.00.00.zip
    Uploaded 887 files on 2025-07-24 17:52:58

---
+++ StartGccBuildEnvironment.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: *************************************************************************************************
+:: Please do NOT change the following settings
+:: *************************************************************************************************
+set PlatformName=IMXRT1060-EVK
+set GettingStartedLink=getting-started-mimxrt1060-evk
+cmd /K Application\Project\GCC\devenv.cmd
